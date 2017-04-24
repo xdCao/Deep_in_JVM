e58bcdcd6a52bdd8939f0db946f3e26f3fc39aff
caohao <705083979@qq.com>
2017-04-24 11:52:34 +0800

    chapter 3.3.2

---
+++ src/chapter3/ReferenceCountingGC.java
@@ -0,0 +1,29 @@
+package chapter3;
+
+/**
+ * Created by xdcao on 2017/4/24.
+ */
+public class ReferenceCountingGC {
+
+    public Object instance=null;
+
+    private static final int _1MB=1024*1024;
+
+    private byte[] bigSize=new byte[2*_1MB];
+
+    public static void main(String[] args){
+
+        ReferenceCountingGC objA=new ReferenceCountingGC();
+        ReferenceCountingGC objB=new ReferenceCountingGC();
+
+        objA.instance=objB;
+        objB.instance=objA;
+
+        objA=null;
+        objB=null;
+
+        System.gc();
+
+    }
+
+}
