9d8d58dfdc43a2b4ba37626efceb15f1cda533a6
caohao <705083979@qq.com>
2017-04-25 23:20:38 +0800

    chapter 7 over

---
+++ src/part3/chapter7/ClassLoaderTest.java
@@ -0,0 +1,39 @@
+package part3.chapter7;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by xdcao on 2017/4/25.
+ */
+public class ClassLoaderTest {
+
+    public static void main(String[] args) throws Exception{
+
+        ClassLoader myLoader=new ClassLoader() {
+            @Override
+            public Class<?> loadClass(String name) throws ClassNotFoundException {
+                try{
+                    String filename=name.substring(name.lastIndexOf(".")+1)+",class";
+                    InputStream is=getClass().getResourceAsStream(filename);
+                    if (is==null){
+                        return super.loadClass(name);
+                    }
+
+                    byte[] b=new byte[is.available()];
+                    is.read(b);
+                    return defineClass(name,b,0,b.length);
+
+                } catch (IOException e) {
+                    throw new ClassNotFoundException(name);
+                }
+            }
+        };
+
+        Object obj=myLoader.loadClass("part3.chapter7.ClassLoaderTest").newInstance();
+        System.out.println(obj.getClass());
+        System.out.println(obj instanceof part3.chapter7.ClassLoaderTest);
+
+    }
+
+}
